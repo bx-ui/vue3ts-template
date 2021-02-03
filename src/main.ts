0c14acad8b97cf4dfacf065b354597d6e10675e5
bx-ui <bx18812096731@163.com>
2021-02-03 08:56:15 +0800

    frist commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+import "lib-flexible"; // 移动端适配
+
+import "@/assets/css/zcm-main.css";
+
+import ElementPlus from "element-plus";
+import "./styles/element-variables.scss";
+
+import "@/utils/auth.ts";
+
+createApp(App)
+  .use(ElementPlus)
+  .use(store)
+  .use(router)
+  .mount("#app");
